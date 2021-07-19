05a2a1ffdba57257b2b313d9b49238e6a5b08dea
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-07-19 14:32:24 +0700

    integrate menu access rules

--- src/@core/layouts/layout-vertical/components/vertical-nav-menu/components/vertical-nav-menu-items/VerticalNavMenuItems.vue
+++ src/@core/layouts/layout-vertical/components/vertical-nav-menu/components/vertical-nav-menu-items/VerticalNavMenuItems.vue
@@ -2,7 +2,7 @@
   <ul>
     <component
       :is="resolveNavItemComponent(item)"
-      v-for="item in items"
+      v-for="item in filteredItems"
       :key="item.header || item.title"
       :item="item"
     />
@@ -28,9 +28,48 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      user: {},
+      menus: [],
+    }
+  },
+  computed: {
+    filteredItems() {
+      const visibleMenus = this.menus.map(menu => {
+        const isAvailableMenuFilter = this.items.filter(
+          item => item.title === menu.name,
+        )
+
+        const isAvailableMenu = isAvailableMenuFilter[0]
+        isAvailableMenu.visible = !!isAvailableMenu
+
+        isAvailableMenu.children.forEach((child, index) => {
+          isAvailableMenu.children[index].visible = !!menu.childrens.filter(
+            val => val.name === child.title,
+          )
+        })
+        return isAvailableMenu
+      })
+
+      return this.user.role_name === 'Management'
+        ? visibleMenus
+        : this.items.map(item => ({ ...item, visible: true }))
+    },
+  },
+  created() {
+    this.user = this.$store.state.auth.userData
+    const positionId = this.user.position_id
+    this.$http
+      .get(
+        `menu/getMyMenuAndAccess?komerce_application_id=4&position_id=${positionId}`,
+      )
+      .then(({ data }) => {
+        this.menus = data.data
+      })
+  },
   setup() {
     provide('openGroups', ref([]))
-
     return {
       resolveNavItemComponent,
     }
