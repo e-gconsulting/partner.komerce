d91a94ded7f906a020de86659a080d951ea97e57
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-08-04 05:35:13 +0700

    fixing issue collapsable

--- src/views/pages/job-role-setting/Collapse.vue
+++ src/views/pages/job-role-setting/Collapse.vue
@@ -1,6 +1,6 @@
 <template>
-  <b-row class="py-1 pl-2 pr-1" v-b-toggle="[`collapse-${index}`]">
-    <b-col md="8">
+  <b-row class="py-1 pl-2 pr-1">
+    <b-col md="8" v-b-toggle="[`collapse-${index}`]">
       <b>{{ item.name }}</b>
     </b-col>
     <b-col md="4" class="text-right">
@@ -16,7 +16,11 @@
         <feather-icon icon="SettingsIcon" class="mr-50" />
         <span class="align-middle">Manage</span>
       </b-button>
-      <feather-icon icon="ChevronDownIcon" class="ml-2 mr-50" />
+      <feather-icon
+        icon="ChevronDownIcon"
+        class="ml-2 mr-50"
+        v-b-toggle="[`collapse-${index}`]"
+      />
     </b-col>
     <b-col md="12" v-if="item.childrens.length > 0">
       <b-collapse :id="`collapse-${index}`" class="mt-2">
