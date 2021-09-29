eb72baabfe59fca515e89b8750f03664339bf5b4
riventusaha <riventus@asuransiastra.com>
2021-09-29 20:59:55 +0700

    update detail data partner

--- src/views/pages/membership/data-partner/Detail.vue
+++ src/views/pages/membership/data-partner/Detail.vue
@@ -3,14 +3,101 @@
     <div v-if="!loadDataAwal">
       <b-card no-body>
         <b-card-body>
-          <div class="wrapper__performa">
-            <h3
+          <div class="d-flex align-items-center">
+            <b-button
+              variant="primary"
+              size="sm"
+              class="mr-50 btn-custom"
+              @click="$router.go(-1)"
+            >
+              <feather-icon
+                size="2x"
+                icon="ChevronLeftIcon"
+              />
+            </b-button>
+            <h4
               class="text-24-bold"
               style="color: #222222;"
             >
-              Validasi Pengguna
-            </h3>
+              Rincian Penarikan Saldo
+            </h4>
           </div>
+          <div class="d-flex justify-content-end">
+            <b-button
+              variant="primary"
+              size="sm"
+              class="mr-50 btn-custom btn-custom--edit"
+              @click="$router.go(-1)"
+            >Edit
+            </b-button>
+          </div>
+
+          <div>
+            <h3
+              class="text-24-bold"
+              style="font-size:20px;color:#222222;"
+            >
+              Informasi Pengguna
+            </h3>
+            <b-row>
+              <b-col md="3">
+                <span>
+                  Nama
+                </span>
+              </b-col>
+              <b-col md="8">
+                <input
+                  placeholder="Nama"
+                >
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col md="3">
+                <span>
+                  Nama
+                </span>
+              </b-col>
+              <b-col md="8">
+                <input
+                  placeholder="Nama"
+                >
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col md="3">
+                <span>
+                  Nama
+                </span>
+              </b-col>
+              <b-col md="8">
+                <input
+                  placeholder="Nama"
+                >
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col md="3">
+                <span>
+                  Nama
+                </span>
+              </b-col>
+              <b-col md="8">
+                <input
+                  placeholder="Nama"
+                >
+              </b-col>
+            </b-row>
+          </div>
+
+          <div>
+            <h4
+              class="text-24-bold"
+              style="color: #222222;"
+            >
+              Informasi Membership Pengguna
+            </h4>
+          </div>
+
         </b-card-body>
       </b-card>
     </div>
@@ -29,13 +116,20 @@
 
 <script>
 import {
+  BRow,
+  BCol,
+  BButton,
   BCard,
   BSpinner,
+  // BFormInput,
   BCardBody,
 } from 'bootstrap-vue'
 
 export default {
   components: {
+    BRow,
+    BCol,
+    BButton,
     BCard,
     BSpinner,
     BCardBody,
@@ -66,5 +160,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.btn-custom{
+  border-radius: 16px;
+  width: 56px;
+  height: 56px;
 
+  &--edit{
+    width: 178px;
+    height: 56px;
+    border-radius: 12px;
+  }
+}
 </style>
