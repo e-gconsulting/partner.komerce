9d946aac82cf37c0748e72b4a6580f29ef81ad74
kang-jenglot <mh1047nr@gmail.com>
2021-10-13 14:22:07 +0700

    Create 2 instance Axios

--- src/views/pages/komship/dashboard/DashboardKomship.vue
+++ src/views/pages/komship/dashboard/DashboardKomship.vue
@@ -8,9 +8,19 @@
 
 <script>
 import BCardActions from '@core/components/b-card-actions/BCardActions.vue'
+import useJwt from '@/auth/jwt/useJwt'
+import httpKomship from '@/libs/http_komship'
 
 export default {
   components: { BCardActions },
+  mounted() {
+    httpKomship.get('v1/bank-account', {
+      headers: { Authorization: `Bearer ${useJwt.getToken()}` },
+    }).then(response => {
+      const { data } = response
+      console.log(data)
+    })
+  },
 
 }
 </script>
