baf8b541e25fff35f2bd95db8ed68a8b01e3bba7
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-10-15 15:51:05 +0700

    feat: create a new instance for http_komship

--- src/views/pages/komship/dashboard/DashboardKomship.vue
+++ src/views/pages/komship/dashboard/DashboardKomship.vue
@@ -1,31 +1,20 @@
 <template>
-  <b-card-actions
-    title="Dashboard"
-    no-actions
-    no-body
-  />
+  <b-card-actions title="Dashboard" no-actions no-body />
 </template>
 
 <script>
 import BCardActions from '@core/components/b-card-actions/BCardActions.vue'
-import useJwt from '@/auth/jwt/useJwt'
-import httpKomship from '@/libs/http_komship'
 
 export default {
   components: { BCardActions },
   mounted() {
-    httpKomship.post('v1/my-profile', {},
-      {
-        headers: { Authorization: `Bearer ${useJwt.getToken()}` },
-      }).then(response => {
+    this.$http_komship.post('v1/my-profile').then(response => {
       const { data } = response
       console.log(data)
     })
   },
-
 }
 </script>
 
 <style>
-
 </style>
