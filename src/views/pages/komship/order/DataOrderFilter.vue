be652134dd0bf79b91115facf16b1334451b1337
Your Name <you@example.com>
2021-12-17 09:49:43 +0700

    revision

--- src/views/pages/komship/order/DataOrderFilter.vue
+++ src/views/pages/komship/order/DataOrderFilter.vue
@@ -250,7 +250,7 @@ export default {
       this.filterForm = {
         date: {},
         product: '',
-        method: null,
+        method: this.selectedExp && this.selectedExp.length > 0 ? this.selectedExp[0] : '',
       }
       this.$emit('onResetForm', this.filterForm)
     },
