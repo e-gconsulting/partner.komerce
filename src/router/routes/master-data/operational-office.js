8097a451d8d445850fa25765a25b9822c5177b27
islakhun24 <51625871+islakhun24@users.noreply.github.com>
2021-12-20 17:04:10 +0700

    asdasd

--- src/router/routes/master-data/operational-office.js
+++ src/router/routes/master-data/operational-office.js
@@ -1,73 +1,73 @@
 const pageName = { singular: 'Kantor Operasional', plural: 'Kantor Operasional' }
 
 export default [
-  // {
-  //   path: '/operational-offices',
-  //   name: 'operational-offices',
-  //   component: () => import('@/views/pages/master-data/operational-office/Table.vue'),
-  //   meta: {
-  //     name: pageName,
-  //     routeCreate: 'operational-offices-create',
-  //     routeEdit: 'operational-offices-edit',
-  //     resource: 'MasterData',
-  //     action: 'manage',
-  //     breadcrumb: [
-  //       {
-  //         text: 'Master Data',
-  //       },
-  //       {
-  //         text: pageName.plural,
-  //         active: true,
-  //       },
-  //     ],
-  //   },
-  // },
-  // // {
-  // //   path: '/operational-offices/create',
-  // //   name: 'operational-offices-create',
-  // //   component: () => import('@/views/pages/master-data/operational-office/Form.vue'),
-  // //   meta: {
-  // //     name: pageName,
-  // //     navActiveLink: 'operational-offices',
-  // //     resource: 'MasterData',
-  // //     action: 'manage',
-  // //     breadcrumb: [
-  // //       {
-  // //         text: 'Master Data',
-  // //       },
-  // //       {
-  // //         text: pageName.plural,
-  // //         route: 'operational-offices',
-  // //       },
-  // //       {
-  // //         text: 'Create',
-  // //         active: true,
-  // //       },
-  // //     ],
-  // //   },
-  // // },
-  // {
-  //   path: '/operational-offices/:id/edit',
-  //   name: 'operational-offices-edit',
-  //   component: () => import('@/views/pages/master-data/operational-office/Form.vue'),
-  //   meta: {
-  //     name: pageName,
-  //     navActiveLink: 'operational-offices',
-  //     resource: 'MasterData',
-  //     action: 'manage',
-  //     breadcrumb: [
-  //       {
-  //         text: 'Master Data',
-  //       },
-  //       {
-  //         text: pageName.plural,
-  //         route: 'operational-offices',
-  //       },
-  //       {
-  //         text: 'Edit',
-  //         active: true,
-  //       },
-  //     ],
-  //   },
-  // },
+  {
+    path: '/operational-offices',
+    name: 'operational-offices',
+    component: () => import('@/views/pages/master-data/operational-office/Table.vue'),
+    meta: {
+      name: pageName,
+      routeCreate: 'operational-offices-create',
+      routeEdit: 'operational-offices-edit',
+      resource: 'MasterData',
+      action: 'manage',
+      breadcrumb: [
+        {
+          text: 'Master Data',
+        },
+        {
+          text: pageName.plural,
+          active: true,
+        },
+      ],
+    },
+  },
+  {
+    path: '/operational-offices/create',
+    name: 'operational-offices-create',
+    component: () => import('@/views/pages/master-data/operational-office/Form.vue'),
+    meta: {
+      name: pageName,
+      navActiveLink: 'operational-offices',
+      resource: 'MasterData',
+      action: 'manage',
+      breadcrumb: [
+        {
+          text: 'Master Data',
+        },
+        {
+          text: pageName.plural,
+          route: 'operational-offices',
+        },
+        {
+          text: 'Create',
+          active: true,
+        },
+      ],
+    },
+  },
+  {
+    path: '/operational-offices/:id/edit',
+    name: 'operational-offices-edit',
+    component: () => import('@/views/pages/master-data/operational-office/Form.vue'),
+    meta: {
+      name: pageName,
+      navActiveLink: 'operational-offices',
+      resource: 'MasterData',
+      action: 'manage',
+      breadcrumb: [
+        {
+          text: 'Master Data',
+        },
+        {
+          text: pageName.plural,
+          route: 'operational-offices',
+        },
+        {
+          text: 'Edit',
+          active: true,
+        },
+      ],
+    },
+  },
 ]
