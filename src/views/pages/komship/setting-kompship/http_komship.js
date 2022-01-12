e2a6600826564598fedd18fec665d581974886e3
islakhun <islakhunnurdzaki22@gmail.com>
2022-01-12 11:46:57 +0700

    env to dev

--- src/views/pages/komship/setting-kompship/http_komship.js
+++ src/views/pages/komship/setting-kompship/http_komship.js
@@ -3,6 +3,6 @@ import useJwt from '@/auth/jwt/useJwt'
 
 const token = useJwt.getToken()
 
-httpKomship.defaults.baseURL = 'https://komship.komerce.id/api'
+httpKomship.defaults.baseURL = 'https://komshipdev.komerce.id/api'
 httpKomship.defaults.headers.common = { Authorization: `bearer ${token}` }
 export default httpKomship
