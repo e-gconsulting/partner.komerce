ed30c67df3b5f7b0665244ae9aecf166209545cb
vtscode <vtscode@gmail.com>
2022-02-22 18:01:56 +0700

    fixing pendapatan penghasilan

---
+++ src/views/pages/komship/keuangan/Penghasilan/Penghasilan.vue
@@ -0,0 +1,6 @@
+<template src="./Penghasilan.html" />
+<script src="./Penghasilan.js" />
+<style lang="scss" src="./Penghasilan.scss" scoped></style>
+<style lang="scss">
+@import '@core/scss/vue/libs/vue-select.scss';
+</style>
