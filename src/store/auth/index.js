b059c01d1b0394b9323678a37f0a2a0281c7d221
ricoerlanggaa <ricoerlanggaa@gmail.com>
2022-03-09 16:19:35 +0700

    Handle Assert Null buat optimasi web

--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -1,7 +1,7 @@
 export default {
   namespaced: true,
   state: {
-    userData: JSON.parse(localStorage.getItem('userData')),
+    userData: JSON.parse(localStorage.getItem('userData')) ? localStorage.getItem('userData') : {},
   },
   mutations: {
     UPDATE_USER_DATA(state, val) {
