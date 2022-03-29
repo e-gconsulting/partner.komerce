459f58fb87cff87619509a74c122cc090a52c5dc
islakhun24 <51625871+islakhun24@users.noreply.github.com>
2022-03-29 13:26:54 +0700

    Update http_komship.js

--- src/libs/http_komship.js
+++ src/libs/http_komship.js
@@ -1,4 +1,4 @@
 import httpKomship from 'axios'
 
-httpKomship.defaults.baseURL = 'https://komshipdev.komerce.id/api'
+httpKomship.defaults.baseURL = 'http://prod-komship.komerce.id/api'
 export default httpKomship
