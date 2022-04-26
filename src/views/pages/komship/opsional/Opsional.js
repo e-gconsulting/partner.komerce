a5ee863e89fcaa8fd295ec4e720d00f43b62a158
kang-jenglot <mh1047nr@gmail.com>
2022-04-26 13:42:31 +0700

    Fitur Pendukung >> Custom Label

--- src/views/pages/komship/opsional/Opsional.js
+++ src/views/pages/komship/opsional/Opsional.js
@@ -2,18 +2,28 @@ import ToastificationContent from '@core/components/toastification/Toastificatio
 import {
   BModal,
   VBModal,
+  BSpinner,
+  BOverlay,
 } from 'bootstrap-vue'
+import { ValidationObserver, ValidationProvider } from 'vee-validate'
+import { required } from '@validations'
+import { heightTransition } from '@core/mixins/ui/transition'
 import Ripple from 'vue-ripple-directive'
 import httpKomship2 from '../setting-kompship/http_komship2'
 
 export default {
   components: {
     BModal,
+    BSpinner,
+    BOverlay,
+    ValidationObserver,
+    ValidationProvider,
   },
   directives: {
     'b-modal': VBModal,
     Ripple,
   },
+  mixins: [heightTransition],
   data() {
     return {
       profile: [],
@@ -21,7 +31,25 @@ export default {
       mutationBank: null,
       customLabel: null,
       isGetting: false,
-      customLabelIsDisable: true,
+      isNotSetActive: true,
+
+      nameSender: '',
+      noHPSender: '',
+      loadingSubmitSender: false,
+      itemsCustomLabel: [],
+      customLabelEditMode: false,
+      fieldAddSender: false,
+      editNameSender: '',
+      editNoHPSender: '',
+      loadingWrapperCustom: false,
+      idCustomLabelEdit: null,
+      senderIsDefault: null,
+      editSenderIsDefault: null,
+      required,
+      loadingDeleteSender: false,
+      messageValidateNumber: '',
+      errorNumber: false,
+      errorNumberEdit: false,
     }
   },
   created() {
@@ -36,12 +64,7 @@ export default {
           this.mutationBank = res.data.data.partner_is_mutation_bank
           this.customLabel = res.data.data.partner_is_custom_label
           this.isGetting = true
-          if (res.data.data.partner_is_custom_label === false) {
-            this.customLabelIsDisable = false
-          } else {
-            this.customLabelIsDisable = true
-          }
-          this.checkCustomLabel()
+          this.getCustomLabel()
         })
         .catch(err => {
           console.log(err)
@@ -83,16 +106,14 @@ export default {
         })
     },
     setCustomLabel() {
-      console.log(this.customLabel)
-      console.log(this.profile.partner_id)
       if (this.customLabel === false) {
         this.$refs['modal-set-custom-label'].show()
-      } else {
+      }
+      if (this.customLabel && this.isNotSetActive) {
         this.$http.post(`/user/partner/setting/isCustomLabel/${this.profile.partner_id}`, {
           is_custom_label: 1,
         })
           .then(response => {
-            console.log(response)
             this.$toast({
               component: ToastificationContent,
               props: {
@@ -129,8 +150,8 @@ export default {
               variant: 'success',
             },
           }, 2000)
-          console.log(response)
           this.customLabel = false
+          this.isNotSetActive = true
           this.$refs['modal-set-custom-label'].hide()
         }).catch(() => {
           this.$toast({
@@ -145,7 +166,171 @@ export default {
         })
     },
     alertCustomLabel() {
+      this.isNotSetActive = false
+      this.customLabel = true
       this.$refs['modal-set-custom-label'].hide()
+    },
+    getCustomLabel() {
+      this.loadingWrapperCustom = true
+      this.$http_komship.get(`/v1/custom-labels/${this.profile.partner_id}`)
+        .then(response => {
+          const { data } = response.data
+          this.itemsCustomLabel = data
+          this.loadingWrapperCustom = false
+        }).catch(err => {
+          console.log(err)
+          this.loadingWrapperCustom = false
+        })
+    },
+    submitSender() {
+      this.loadingSubmitSender = true
+      this.$refs.formRulesAdd.validate().then(success => {
+        if (success) {
+          const formData = new FormData()
+          formData.append('name_label', this.nameSender)
+          formData.append('no_hp', this.noHPSender)
+          formData.append('partner_id', this.profile.partner_id)
+          formData.append('is_default', this.senderIsDefault ? 1 : 0)
+          this.$http_komship.post('/v1/custom-labels/store', formData)
+            .then(response => {
+              this.$toast({
+                component: ToastificationContent,
+                props: {
+                  title: 'Success',
+                  icon: 'CheckIcon',
+                  text: response.data.message,
+                  variant: 'success',
+                },
+              }, 2000)
+              this.getCustomLabel()
+              this.fieldAddSender = false
+              this.loadingSubmitSender = false
+            }).catch(err => {
+              this.$toast({
+                component: ToastificationContent,
+                props: {
+                  title: 'Failure',
+                  icon: 'AlertCircleIcon',
+                  text: err.response.data.message,
+                  variant: 'danger',
+                },
+              }, 2000)
+              this.loadingSubmitSender = false
+            })
+        } else {
+          this.loadingSubmitSender = false
+        }
+      })
+    },
+    updateSenderLabel() {
+      this.loadingSubmitSender = true
+      // eslint-disable-next-line dot-notation
+      this.$refs['formEditRules'][0].validate().then(success => {
+        if (success) {
+          const formData = new FormData()
+          formData.append('name_label', this.editNameSender)
+          formData.append('no_hp', this.editNoHPSender)
+          formData.append('is_default', this.editSenderIsDefault ? 1 : 0)
+          formData.append('_method', 'put')
+          this.$http_komship.post(`/v1/custom-labels/update/${this.idCustomLabelEdit}`, formData)
+            .then(response => {
+              this.$toast({
+                component: ToastificationContent,
+                props: {
+                  title: 'Success',
+                  icon: 'CheckIcon',
+                  text: response.data.message,
+                  variant: 'success',
+                },
+              }, 2000)
+              this.getCustomLabel()
+              this.loadingSubmitSender = false
+              this.customLabelEditMode = false
+            }).catch(err => {
+              this.$toast({
+                component: ToastificationContent,
+                props: {
+                  title: 'Failure',
+                  icon: 'AlertCircleIcon',
+                  text: err.response.message,
+                  variant: 'danger',
+                },
+              }, 2000)
+              this.loadingSubmitSender = false
+            })
+        } else {
+          this.loadingSubmitSender = false
+        }
+      })
+    },
+    deleteSenderLabel(data) {
+      this.loadingDeleteSender = true
+      this.$http_komship.delete(`/v1/custom-labels/delete/${data.id}`)
+        .then(response => {
+          this.$toast({
+            component: ToastificationContent,
+            props: {
+              title: 'Success',
+              icon: 'CheckIcon',
+              text: response.data.message,
+              variant: 'success',
+            },
+          }, 2000)
+          this.getCustomLabel()
+          this.loadingDeleteSender = false
+          this.customLabelEditMode = false
+        }).catch(err => {
+          this.$toast({
+            component: ToastificationContent,
+            props: {
+              title: 'Failure',
+              icon: 'AlertCircleIcon',
+              text: err.response.message,
+              variant: 'danger',
+            },
+          }, 2000)
+          this.loadingDeleteSender = false
+        })
+    },
+    addSender() {
+      this.customLabelEditMode = false
+      this.fieldAddSender = true
+    },
+    removeFieldAddSender() {
+      this.fieldAddSender = false
+    },
+    editSenderActive(data) {
+      this.errorNumberEdit = false
+      this.editNameSender = data.name_label
+      this.editNoHPSender = data.no_hp
+      this.idCustomLabelEdit = data.id
+      if (data.is_default === 1) {
+        this.editSenderIsDefault = true
+      } else {
+        this.editSenderIsDefault = false
+      }
+      this.customLabelEditMode = true
+    },
+    editSenderNoActive() {
+      this.customLabelEditMode = false
+    },
+    checkNumber() {
+      if (this.noHPSender.length < 9) {
+        this.errorNumber = true
+        this.messageValidateNumber = '*minimal 9 digit dan hanya berupa angka.'
+      } else {
+        this.errorNumber = false
+        this.messageValidateNumber = ''
+      }
+    },
+    checkEditNumber() {
+      if (this.editNoHPSender.length < 9) {
+        this.errorNumberEdit = true
+        this.messageValidateNumber = '*minimal 9 digit dan hanya berupa angka.'
+      } else {
+        this.errorNumber = false
+        this.messageValidateNumber = ''
+      }
     },
   },
 }
