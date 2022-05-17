af7489b49fc03e55c695ed2288e624b2c9190c96
kang-jenglot <mh1047nr@gmail.com>
2022-05-17 16:18:07 +0700

    Rendering data detail ticket user

--- src/views/pages/komship/detail-ticketing-user/DetailTicketing.js
+++ src/views/pages/komship/detail-ticketing-user/DetailTicketing.js
@@ -30,6 +30,12 @@ export default {
           this.orderStatus = data.order_status
           this.ticketNo = data.ticket_no
           this.ticketType = data.ticket_type
+          this.customerName = data.customer_name
+          this.description = data.description
+          this.seller = data.partner_name
+          this.ekspedisi = data.shipping
+          this.noResi = data.no_resi
+          this.file = data.file
         })
         .catch(err => {
           console.log(err)
