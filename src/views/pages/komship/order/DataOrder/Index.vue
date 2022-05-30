c2b21b3fcf5da0f740f3c20ee609bd75b61cfc4b
islakhun <islakhunnurdzaki22.magang@doku.com>
2022-05-30 10:55:30 +0700

    download

--- src/views/pages/komship/order/DataOrder/Index.vue
+++ src/views/pages/komship/order/DataOrder/Index.vue
@@ -1,31 +1,297 @@
 <template>
   <b-card>
     <div class="d-flex justify-between mb-2">
-      <h3 class="font-bold">
-        Data Order
-      </h3>
-      <div>
-        <b-button
-          v-if="tabIndex === 1"
-          variant="primary"
-          size="sm"
-          class="mr-1"
-          to="ajukan-pickup"
-        >
-          Ajukan Pickup
-        </b-button>
-        <b-dropdown
-          id="dropdown-1"
-          variant="outline-primary"
-          size="sm"
-          right
-          text="Export"
-        >
-          <b-dropdown-item>Export Excel</b-dropdown-item>
-          <b-dropdown-item>Export CSV</b-dropdown-item>
-        </b-dropdown>
-      </div>
+      <b-container>
+        <b-row>
+          <b-col>
+            <h3 class="font-bold">
+              Data Order
+            </h3>
+          </b-col>
+          <b-col>
+            <div class="d-flex justify-content-end">
+              <b-button
+                v-if="tabIndex === 1"
+                variant="primary"
+                size="md"
+                class="mr-1"
+                to="ajukan-pickup"
+              >
+                Ajukan Pickup
+              </b-button>
+              <b-button
+                id="btn-export"
+                v-b-modal.modalExport
+                variant="outline-primary"
+                size="md"
+                right
+              >
+                Download
+              </b-button>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
     </div>
+    <b-row>
+      <b-modal
+        id="modalExport"
+        centered
+        :size="'lg'"
+        no-close-on-backdrop
+        :header-bg-variant="'white'"
+        :header-class="'no-border'"
+        ok-title="Download"
+        ok-variant="primary"
+        cancel-title="Batalkan"
+        cancel-variant="outline-primary"
+        :footer-class="'no-border'"
+        :content-class="'no-border'"
+        hide-header
+      >
+        <template>
+          <b-container class="text-center col">
+            <b-row class="justify-content-end">
+              <b-icon-x-circle
+                style="width: 24px; height: 24px; cursor: pointer; "
+                class="close-button-on-popup-pickup text-right"
+                end
+                @click="handleClosePopUp"
+              />
+            </b-row>
+            <b-row class="mt-2 justify-content-center">
+              <h3>
+                Download Data Order
+              </h3>
+            </b-row>
+            <b-row class="justify-content-center">
+              Kustomisasi data order yang ingin kamu download
+            </b-row>
+            <b-row class="mt-2">
+              <b-col
+                sm="12"
+                md="3"
+              >
+                <b-row class="border-b-2 pb-1 font-weight-bold">
+                  Tanggal Order
+                </b-row>
+                <b-row class="my-1 mr-0 mr-md-2">
+                  <date-range-picker
+                    ref="picker"
+                    v-model="dateRange"
+                    :opens="opens"
+                    :locale-data="{
+                      direction: 'ltr',
+                      format: 'mm/dd/yyyy',
+                      separator: ' - ',
+                      applyLabel: 'Simpan',
+                      cancelLabel: 'Batal',
+                      weekLabel: 'M',
+                      customRangeLabel: 'Custom Range',
+                      daysOfWeek: ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
+                      monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'],
+                      firstDay: 0
+                    }"
+                    :min-date="minDate"
+                    :max-date="maxDate"
+                    :single-date-picker="singleDatePicker"
+                    :time-picker="timePicker"
+                    :time-picker24hour="timePicker24Hour"
+                    :show-week-numbers="showWeekNumbers"
+                    :show-dropdowns="showDropdowns"
+                    :auto-apply="autoApply"
+                    :linked-calendars="linkedCalendars"
+                    :date-format="dateFormat"
+                    @update="updateValues"
+                    @toggle="logEvent('event: open', $event)"
+                    @start-selection="logEvent('event: startSelection', $event)"
+                    @finish-selection="logEvent('event: finishSelection', $event)"
+                  >
+                    <template #ranges="ranges">
+                      <div class="ranges">
+                        <ul>
+                          <li @click="ranges[0].clickRange(ranges[0].range)">
+                            <b>Hari ini</b>
+                          </li>
+                          <li @click="ranges[0].clickRange(ranges[0].range)">
+                            <b>7 hari terakhir</b>
+                          </li>
+                          <li @click="ranges[0].clickRange(ranges[0].range)">
+                            <b>30 hari terakhir</b>
+                          </li>
+                          <li @click="ranges[0].clickRange(ranges[0].range)">
+                            <b>Custom Tanggal</b>
+                          </li>
+                        <!-- <li
+                              v-for="(range, name, index) in ranges.ranges"
+                              :key="name"
+                              @click="ranges.clickRange(range)"
+                            >
+                              <b v->{{ getLabelDateRange(index) }}</b>
+                            </li> -->
+                        </ul>
+                      </div>
+                    </template>
+                  </date-range-picker>
+                </b-row>
+              </b-col>
+              <b-col
+                sm="12"
+                md="3"
+              >
+                <b-row class="border-b-2 pb-1 mt-2 mt-md-0 font-weight-bold">
+                  Metode Pembayaran
+                </b-row>
+                <b-row>
+                  <div>
+                    <div class="my-1">
+                      <b-form-checkbox
+                        id="checkbox-1"
+                        v-model="status"
+                        name="checkbox-1"
+                        class="text-left"
+                        value="accepted"
+                        unchecked-value="not_accepted"
+                      >
+                        COD
+                      </b-form-checkbox>
+                    </div>
+                    <div class="my-1">
+                      <b-form-checkbox
+                        id="checkbox-1"
+                        v-model="status"
+                        name="checkbox-1"
+                        class="text-left"
+                        value="accepted"
+                        unchecked-value="not_accepted"
+                      >
+                        Bank Transfer
+                      </b-form-checkbox>
+                    </div>
+                  </div>
+                </b-row>
+              </b-col>
+              <b-col
+                sm="12"
+                md="3"
+              >
+                <b-row class="border-b-2 pb-1  mt-2 mt-md-0 font-weight-bold">
+                  Status
+                </b-row>
+                <b-row>
+                  <div class="my-1">
+                    <b-form-checkbox
+                      id="checkbox-1"
+                      v-model="status"
+                      name="checkbox-1"
+                      class="text-left"
+                      value="accepted"
+                      unchecked-value="not_accepted"
+                    >
+                      Order Dibuat
+                    </b-form-checkbox>
+                    <div class="my-1">
+                      <b-form-checkbox
+                        id="checkbox-1"
+                        v-model="status"
+                        class="text-left"
+                        name="checkbox-1"
+                        value="accepted"
+                        unchecked-value="not_accepted"
+                      >
+                        Dipacking
+                      </b-form-checkbox>
+                    </div>
+                    <div class="my-1">
+                      <b-form-checkbox
+                        id="checkbox-1"
+                        v-model="status"
+                        name="checkbox-1"
+                        class="text-left"
+                        value="accepted"
+                        unchecked-value="not_accepted"
+                      >
+                        Dikirim
+                      </b-form-checkbox>
+                    </div>
+                    <div class="my-1">
+                      <b-form-checkbox
+                        id="checkbox-1"
+                        v-model="status"
+                        name="checkbox-1"
+                        class="text-left"
+                        value="accepted"
+                        unchecked-value="not_accepted"
+                      >
+                        Diterima
+                      </b-form-checkbox>
+                    </div>
+                    <div class="my-1">
+                      <b-form-checkbox
+                        id="checkbox-1"
+                        v-model="status"
+                        name="checkbox-1"
+                        class="text-left"
+                        value="accepted"
+                        unchecked-value="not_accepted"
+                      >
+                        Retur
+                      </b-form-checkbox>
+                    </div>
+                  </div>
+                </b-row>
+              </b-col>
+              <b-col
+                sm="12"
+                md="3"
+              >
+                <b-row class="border-b-2 pb-1  mt-2 mt-md-0 font-weight-bold">
+                  Expedisi
+                </b-row>
+                <b-row>
+                  <div class="my-1">
+                    <b-form-checkbox
+                      id="checkbox-1"
+                      v-model="status"
+                      name="checkbox-1"
+                      class="text-left"
+                      value="accepted"
+                      unchecked-value="not_accepted"
+                    >
+                      JNE
+                    </b-form-checkbox>
+                    <div class="my-1">
+                      <b-form-checkbox
+                        id="checkbox-1"
+                        v-model="status"
+                        class="text-left"
+                        name="checkbox-1"
+                        value="accepted"
+                        unchecked-value="not_accepted"
+                      >
+                        SiCepat
+                      </b-form-checkbox>
+                    </div>
+                    <div class="my-1">
+                      <b-form-checkbox
+                        id="checkbox-1"
+                        v-model="status"
+                        name="checkbox-1"
+                        class="text-left"
+                        value="accepted"
+                        unchecked-value="not_accepted"
+                      >
+                        IDExpress
+                      </b-form-checkbox>
+                    </div>
+                  </div>
+                </b-row>
+              </b-col>
+            </b-row>
+          </b-container>
+        </template>
+      </b-modal>
+    </b-row>
     <b-tabs
       v-model="tabIndex"
       fill
@@ -92,9 +358,10 @@
 </template>
 <script>
 import {
-  BCard, BTabs, BTab, BDropdown, BDropdownItem, BButton, BBadge,
+  BCard, BTabs, BTab, BButton, BBadge, BCol, BRow, BContainer, BIconXCircle,
 } from 'bootstrap-vue'
 import ToastificationContent from '@core/components/toastification/ToastificationContent.vue'
+import DateRangePicker from 'vue2-daterange-picker'
 import All from './List/All.vue'
 import Created from './List/Created.vue'
 import Packing from './List/Packing.vue'
@@ -104,9 +371,22 @@ import Retur from './List/Retur.vue'
 
 export default {
   components: {
-    BCard, BTabs, BTab, BDropdown, BDropdownItem, All, Created, Packing, Send, Received, Retur, BButton, BBadge,
+    BCard, BTabs, BTab, All, Created, Packing, Send, Received, Retur, BButton, BBadge, BCol, BContainer, BRow, BIconXCircle, DateRangePicker,
+  },
+  filters: {
+    dateCell(value) {
+      const dt = new Date(value)
+
+      return dt.getDate()
+    },
+    date(val) {
+      return val ? val.toLocaleString() : ''
+    },
   },
   data() {
+    const startDate = new Date()
+    const endDate = new Date()
+    endDate.setDate(endDate.getDate() + 6)
     const tabs = ['semua', 'order-dibuat', 'dipacking', 'dikirim', 'diterima', 'retur']
     return {
       tabIndex: tabs.indexOf(this.$route.query.tab),
@@ -115,6 +395,21 @@ export default {
       totalAjukan: null,
       totalPacking: null,
       totalKirim: null,
+      dateRange: { startDate, endDate },
+      // ranges: [
+      //   {
+      //     name: 'Real Time',
+      //   },
+      //   {
+      //     name: '7 hari terakhir',
+      //   },
+      //   {
+      //     name: '30 hari terakhir',
+      //   },
+      //   {
+      //     name: 'Custom Tanggal',
+      //   },
+      // ],
     }
   },
   watch: {
@@ -177,6 +472,67 @@ export default {
         })
       })
     },
+    handleClosePopUp() {
+      this.$root.$emit('bv::hide::modal', 'modalExport')
+    },
+    getLabelDateRange(index) {
+      if (index === 0) {
+        return 'Real Time'
+      }
+
+      if (index === 1) {
+        return '7 hari terakhir'
+      }
+
+      if (index === 2) {
+        return '30 hari terakhir'
+      }
+
+      if (index === 3) {
+        return 'Custom Tanggal'
+      }
+
+      return ''
+    },
+    // showModalExport() {
+    //   this.$bvModal.msgBoxOk('Data was submitted successfully', {
+    //   //   title: <div>
+    //   //   <b-icon-x-circle
+    //   //   class="close-button-on-popup-pickup"
+    //   //   @click="handleClosePopUp"
+    //   // /></div>,
+    //     title: 'Export',
+
+    //     size: 'sm',
+    //     buttonSize: 'sm',
+    //     okVariant: 'success',
+
+    //     headerCloseLabel: 'Close',
+    //     bodyClass: 'end',
+    //     headerClass: 'p-2 border-bottom-0 bg-white',
+    //     footerClass: 'p-2 border-top-0',
+    //     centered: true,
+    //   })
+    //     .then(value => {
+    //       // this.boxTwo = value
+    //     })
+    //     .catch(err => {
+    //       // An error occurred
+    //     })
+    // },
   },
 }
 </script>
+<style lang="scss" scoped>
+.slot {
+  background-color: #aaa;
+  padding: 0.5rem;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.text-black {
+  color: #000;
+}
+</style>
