9ee413f0db1e57388cca1990799208f7f71dde56
DESKTOP-TJEQ29F\Komerce <islakhunnurdzaki24@gmail.com>
2022-06-08 10:35:07 +0700

    fixck

--- src/views/pages/komship/order/DataOrder/Index.vue
+++ src/views/pages/komship/order/DataOrder/Index.vue
@@ -241,7 +241,7 @@
                       class="text-left"
                       value="Diajukan"
                     >
-                      Order Dibuat
+                      Order Dibuats
                     </b-form-checkbox>
                     <div class="my-1">
                       <b-form-checkbox
