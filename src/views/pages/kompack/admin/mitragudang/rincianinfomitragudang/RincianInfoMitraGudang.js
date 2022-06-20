1d801d796748a3b52db72e7421af20452f7283e2
herlambangajiprasetyo <herlambangajiprasetyo36@gmail.com>
2022-06-20 08:27:25 +0700

    mitra-gudang

--- src/views/pages/kompack/admin/mitragudang/rincianinfomitragudang/RincianInfoMitraGudang.js
+++ src/views/pages/kompack/admin/mitragudang/rincianinfomitragudang/RincianInfoMitraGudang.js
@@ -92,29 +92,6 @@ export default {
     fetchData() {
       this.$http_kompack(`/kompack/warehouse/information/${this.$route.params.id}`)
         .then(({ data }) => {
-          /*
-          "data": {
-            "id": "26",
-            "owner": "amskdmsakdm",
-            "phone_number": "128957192372918",
-            "partner_verification": "Terverifikasi",
-            "image_logo_url": "https:\/\/kompackdev.komerce.id\/warehouse_logo\/1649751302.Test_image_2.jpg",
-            "warehouse_name": "amskdmsakdm",
-            "avability": "Penuh",
-            "pic_phone": "12849817294",
-            "description": "mkasdmaksdm",
-            "detail_address": ", AMPENAN, MATARAM, NUSA TENGGARA BARAT (NTB), 83511",
-            "total_order": 0,
-            "join_date": "2022-04-12",
-            "image_warehouse": [{
-                "id": 9,
-                "warehouse_id": 12,
-                "image_url": "https:\/\/kompackdev.komerce.id\/warehouse_images\/1649751303-Test_image_2.jpg",
-                "created_at": "2022-04-12T08:15:03.000000Z",
-                "updated_at": "2022-04-12T08:15:03.000000Z"
-            }]
-          }
-          */
           this.detailInfo = { ...data.data }
           this.imagesone = data.data.image_warehouse[0].image_url
           this.imagesothers = [...data.data.image_warehouse].splice(0, 8).map(x => x.image_url)
