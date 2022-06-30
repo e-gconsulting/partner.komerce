4f1343332ae7ed0288335a6af28275948f0cf8f1
isnursamsi <mh1047nr@gmail.com>
2022-06-30 16:57:31 +0700

    re-commit

--- src/views/pages/komship/order/DataOrder/DetailOrder.vue
+++ src/views/pages/komship/order/DataOrder/DetailOrder.vue
@@ -840,7 +840,7 @@ export default {
         this.listAwb += `<span class="font-bold">${items.desc}</span>`
         this.listAwb += '</div><br>'
         console.log('items', items)
-        if (items.status === 'Pickup' && items.send_wa === 1) {
+        if (items.send_wa === 1) {
           this.listAwb += '<div class="d-flex relative p-1" style="margin-left:50px;border:1px solid #E2E2E2;border-radius:4px;margin-bottom:-50px;max-width:400px">'
           this.listAwb += `<img src="${require('@/assets/images/icons/whatsapp-notif.svg')}">`
           this.listAwb += '<span class="my-auto" style="margin-left:6px">Pemberitahuan pemberangkatan telah terkirim ke WA Pelanggan</span>'
