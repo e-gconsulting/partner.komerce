0f1eadd1cc7f2a2a74a21db350487bd177e3dbc2
khoerul umam <khoerulumam0723@gmail.com>
2022-09-29 11:45:57 +0700

    forgot tautan lupa password partner kompack

--- src/views/pages/authentication/ForgotPassword/Partnerlupapassword/ForgotPassword.vue
+++
@@ -1,6 +0,0 @@
-<template src="./ForgotPassword.html" />
-<script src="./ForgotPassword.js" />
-<style lang="scss">
-@import '~@core/scss/vue/pages/page-auth.scss';
-</style>
-<style lang="scss" src="./ForgotPassword.scss" scoped />
