db17eb4f752918806869b5b0427c08ea0fa05818
khoerul umam <khoerulumam0723@gmail.com>
2022-10-26 15:18:30 +0700

    search gudangku

--- src/views/pages/kompack/gudang/SearchGudangku.vue
+++ src/views/pages/kompack/gudang/SearchGudangku.vue
@@ -39,12 +39,29 @@
                   <b-card-img
                     src="https://picsum.photos/400/400/?image=20"
                     alt="Image"
-                    class="rounded-2"
+                    class="rounded-1"
                   />
                 </b-col>
-                <card-table md="6">
-                    sdsdsd
-                  </card-table>
+              </b-row>
+            </b-card>
+
+          </div>
+        </template>
+        <template>
+          <div>
+            <b-card
+              no-body
+              class="overflow-hidden"
+              style="max-width: 350px;"
+            >
+              <b-row no-gutters>
+                <b-col md="6">
+                  <b-card-img
+                    src="https://picsum.photos/400/400/?image=20"
+                    alt="Image"
+                    class="rounded-1"
+                  />
+                </b-col>
               </b-row>
             </b-card>
           </div>
@@ -61,44 +78,9 @@
                   <b-card-img
                     src="https://picsum.photos/400/400/?image=20"
                     alt="Image"
-                    class="rounded-2"
+                    class="rounded-1"
                   />
                 </b-col>
-                <!-- <b-col md="6">
-                  <b-card-body title="Horizontal Card">
-                    <b-card-text>
-                      This is a wider card with supporting text as a natural lead-in to additional content.
-                      This content is a little bit longer.
-                    </b-card-text>
-                  </b-card-body>
-                </b-col> -->
-              </b-row>
-            </b-card>
-          </div>
-        </template>
-        <template>
-          <div>
-            <b-card
-              no-body
-              class="overflow-hidden"
-              style="max-width: 350px;"
-            >
-              <b-row no-gutters>
-                <b-col md="6">
-                  <b-card-img
-                    src="https://picsum.photos/400/400/?image=20"
-                    alt="Image"
-                    class="rounded-2"
-                  />
-                </b-col>
-                <!-- <b-col md="6">
-                  <b-card-body title="Horizontal Card">
-                    <b-card-text>
-                      This is a wider card with supporting text as a natural lead-in to additional content.
-                      This content is a little bit longer.
-                    </b-card-text>
-                  </b-card-body>
-                </b-col> -->
               </b-row>
             </b-card>
           </div>
