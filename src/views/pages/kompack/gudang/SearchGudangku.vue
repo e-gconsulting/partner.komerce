2441eb92de904ad166313224cbcd80dca9a7d7f0
khoerul umam <khoerulumam0723@gmail.com>
2022-10-26 09:55:13 +0700

    partner kompack search gudang table gudang

--- src/views/pages/kompack/gudang/SearchGudangku.vue
+++ src/views/pages/kompack/gudang/SearchGudangku.vue
@@ -39,17 +39,12 @@
                   <b-card-img
                     src="https://picsum.photos/400/400/?image=20"
                     alt="Image"
-                    class="rounded-0"
+                    class="rounded-2"
                   />
                 </b-col>
-                <b-col md="6">
-                  <b-card-body title="Horizontal Card">
-                    <b-card-text>
-                      This is a wider card with supporting text as a natural lead-in to additional content.
-                      This content is a little bit longer.
-                    </b-card-text>
-                  </b-card-body>
-                </b-col>
+                <card-table md="6">
+                    sdsdsd
+                  </card-table>
               </b-row>
             </b-card>
           </div>
@@ -66,17 +61,17 @@
                   <b-card-img
                     src="https://picsum.photos/400/400/?image=20"
                     alt="Image"
-                    class="rounded-0"
+                    class="rounded-2"
                   />
                 </b-col>
-                <b-col md="6">
+                <!-- <b-col md="6">
                   <b-card-body title="Horizontal Card">
                     <b-card-text>
                       This is a wider card with supporting text as a natural lead-in to additional content.
                       This content is a little bit longer.
                     </b-card-text>
                   </b-card-body>
-                </b-col>
+                </b-col> -->
               </b-row>
             </b-card>
           </div>
@@ -93,17 +88,17 @@
                   <b-card-img
                     src="https://picsum.photos/400/400/?image=20"
                     alt="Image"
-                    class="rounded-0"
+                    class="rounded-2"
                   />
                 </b-col>
-                <b-col md="6">
+                <!-- <b-col md="6">
                   <b-card-body title="Horizontal Card">
                     <b-card-text>
                       This is a wider card with supporting text as a natural lead-in to additional content.
                       This content is a little bit longer.
                     </b-card-text>
                   </b-card-body>
-                </b-col>
+                </b-col> -->
               </b-row>
             </b-card>
           </div>
