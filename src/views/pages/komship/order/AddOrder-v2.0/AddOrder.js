d51795a241aada04f2ac6ae4aeb95e424ebab242
Herlambang Aji Prasetyo <96033617+herlambangajiprasetyo@users.noreply.github.com>
2022-12-21 16:46:21 +0700

    Rev Add order manual

--- src/views/pages/komship/order/AddOrder-v2.0/AddOrder.js
+++ src/views/pages/komship/order/AddOrder-v2.0/AddOrder.js
@@ -1592,15 +1592,26 @@ export default {
         || text.charAt(x) === ';'
         || text.charAt(x) === '"'
         || text.charAt(x) === '&'
+        || text.charAt(x) === '*'
+        || text.charAt(x) === '#'
+        || text.charAt(x) === '&'
+        || text.charAt(x) === '%'
+        || text.charAt(x) === '['
+        || text.charAt(x) === ']'
+        || text.charAt(x) === '>'
+        || text.charAt(x) === '<'
+        || text.charAt(x) === '|'
         || text.charAt(x) === '+'
+        || text.charAt(x) === '?'
+        || text.charAt(x) === '!'
         || text.charAt(x) === ':') {
           string += text.charAt(x)
         } else {
-          string += text.charAt(x).replace(/[^A-Za-z-0-9_ , - .]/g, '')
+          string += text.charAt(x).replace(/[^A-Za-z-0-9_ , - . / ( )]/g, '')
         }
       }
       this.customerAddress = string
-      if (text.match(/[^A-Za-z-0-9_ , - .]/g)) {
+      if (text.match(/[^A-Za-z-0-9_ , - . / ( )]/g)) {
         this.messageErrorAddressDetail = true
       } else {
         this.messageErrorAddressDetail = false
