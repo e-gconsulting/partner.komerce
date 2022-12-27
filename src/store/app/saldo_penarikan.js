5373fb1148be182df7797e86215252e95a19c6e3
Is Nursamsi <75351550+isnursamsi@users.noreply.github.com>
2022-12-27 11:15:46 +0700

    [Partner] Rincian Penarikan V 2.0

--- src/store/app/saldo_penarikan.js
+++ src/store/app/saldo_penarikan.js
@@ -36,12 +36,10 @@ export default {
       state.totalSaldo = totalSaldo
     },
     UPDATE_DETAIL_SALDO(state, detailSaldo) {
-      state.dateStart = moment(
-        detailSaldo.previous_request_withdrawal_date,
-      ).format('DD MMMM YYYY')
+      state.dateStart = moment(new Date(detailSaldo.previous_request_withdrawal_date)).format('YYYY-MM-DD')
       state.notes = detailSaldo.notes
       state.previous_request_withdrawal_date = moment(detailSaldo.previous_request_withdrawal_date).format('DD MMMM YYYY')
-      state.dateEnd = moment(detailSaldo.created_at).format('DD MMMM YYYY')
+      state.dateEnd = moment(new Date(detailSaldo.created_at)).format('YYYY-MM-DD')
       state.timeEnd = moment(detailSaldo.created_at).format('HH:mm')
       state.previous_request_withdrawal_time = moment(detailSaldo.previous_request_withdrawal_date).format('HH:mm')
       state.nominalPenarikan = detailSaldo.nominal
@@ -50,7 +48,7 @@ export default {
     UPDATE_RINCIAN_SALDO(state, rincianSaldos) {
       state.list_item_rincian_penarikan = rincianSaldos.data
       state.table.totalRows = rincianSaldos.total
-      state.rincianSaldos = rincianSaldos.map(item => ({
+      state.rincianSaldos = rincianSaldos.data.map(item => ({
         tanggal: moment(new Date(item.order_date)).format('DD-MM-YYYY'),
         jenisOrder: item.order_type || '-',
         retur: false,
@@ -91,6 +89,7 @@ export default {
       }
     },
     async getRincianSaldo({ commit, state, rootState }, params) {
+      console.log(params)
       state.table.perPage = params
       try {
         const partnerId = rootState.auth.userData.partner_detail.id
@@ -98,10 +97,10 @@ export default {
           'v1/partner/order-transaction-balance',
           {
             params: {
-              start_date: formatYmd(state.dateStart),
-              end_date: formatYmd(state.dateEnd),
+              start_date: state.dateStart,
+              end_date: state.dateEnd,
               page: state.table.currentPage,
-              total_per_page: state.table.perPage,
+              limits: state.table.perPage,
             },
           },
         )
